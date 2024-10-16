b139cfe74172a55fcdd28fdc884726ca7c977b70
j4821_s <sonhm48021@gmail.com>
2024-10-16 15:54:23 +0900

    solve: BOJ_1541

---
+++ java_practice/Rank3/_1541_LostParentheses.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1541_LostParentheses {
+    public static void main(String[ ] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().trim().split("");
+        int sum = 0; boolean flag = false;
+
+        StringBuilder sb = new StringBuilder();
+        for(String s : str) {
+            try {
+                sb.append(Integer.parseInt(s));
+            } catch(NumberFormatException e) {
+                if(flag) {
+                    sum -= Integer.parseInt(sb.toString());
+                } else {
+                    sum += Integer.parseInt(sb.toString());
+                }
+
+                sb.setLength(0);
+            }
+
+            if(s.equals("-")) flag = true;
+        }
+
+        if(flag) {
+            sum -= Integer.parseInt(sb.toString());
+        } else {
+            sum += Integer.parseInt(sb.toString());
+        }
+
+        System.out.println(sum);
+    }
+}
--- java_practice/untitled/Main.java
+++ java_practice/untitled/Main.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[ ] args) throws IOException {
+
     }
 }
