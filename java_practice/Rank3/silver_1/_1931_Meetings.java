c4424270bd06b22430e69bc6332b573fc3238191
j4821_s <sonhm48021@gmail.com>
2024-11-20 15:37:05 +0900

    solve: BOJ_1931

---
+++ java_practice/Rank3/silver_1/_1931_Meetings.java
@@ -0,0 +1,32 @@
+package silver_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _1931_Meetings {
+    private static int compareMeetings(int[] a, int[] b) {
+        return a[1] != b[1] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]);
+    }
+    public static void main(String[ ] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int times = Integer.parseInt(br.readLine());
+        int[][] meets = new int[times][2];
+        for(int i=0; i<times; i++) {
+            String[] meetStr = br.readLine().trim().split(" ");
+            meets[i][0] = Integer.parseInt(meetStr[0]);
+            meets[i][1] = Integer.parseInt(meetStr[1]);
+        }
+        Arrays.sort(meets, _1931_Meetings::compareMeetings);
+        int end = meets[0][1], cnt = 1;
+        for(int i=1; i<meets.length; i++) {
+            if(meets[i][0] < end) continue;
+
+            cnt++;
+            end = meets[i][1];
+        }
+
+        System.out.println(cnt);
+    }
+}
