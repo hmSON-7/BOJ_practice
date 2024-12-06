2b8a7dcfe06487a9a9c593b8347f20dff9335ac9
j4821_s <sonhm48021@gmail.com>
2024-12-06 11:53:34 +0900

    refactor: BOJ_11053 클래스명 수정

--- java_practice/non_class/silver_2/_11053_TheLongestIncreasingSequence.java
+++ java_practice/non_class/silver_2/_11053_TheLongestIncreasingSequence.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class TheLongestIncreasingSequence {
+public class _11053_TheLongestIncreasingSequence {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n  = Integer.parseInt(br.readLine());
