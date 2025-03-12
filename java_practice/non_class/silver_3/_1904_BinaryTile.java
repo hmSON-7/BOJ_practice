a21f072367dcba9c5da20d547e3f74964495b6c6
hmSON-7 <sonhm48021@gmail.com>
2025-03-12 12:33:25 +0900

    solve: BOJ_1904

---
+++ java_practice/non_class/silver_3/_1904_BinaryTile.java
@@ -0,0 +1,18 @@
+package silver_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1904_BinaryTile {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+        }
+        System.out.println(dp[n]);
+    }
+}
