315962431e53874f6f582b7b3874e8a7cd465944
hmSON-7 <sonhm48021@gmail.com>
2025-03-25 15:41:18 +0900

    solve: BOJ_4779

---
+++ java_practice/non_class/silver_3/_4779_CantorSet.java
@@ -0,0 +1,29 @@
+package silver_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _4779_CantorSet {
+    static StringBuilder sb = new StringBuilder();
+    static String[] dp = new String[13];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        dp[0] = "-";
+        String str;
+        while((str = br.readLine()) != null && !str.isEmpty()) {
+            int n = Integer.parseInt(str);
+            sb.append(cantor(n)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static String cantor(int n) {
+        if (n == 0) return "-";
+        if (dp[n] != null) return dp[n];
+        String prev = cantor(n - 1);
+        String spaces = " ".repeat(prev.length());
+        dp[n] = prev + spaces + prev;
+        return dp[n];
+    }
+}
