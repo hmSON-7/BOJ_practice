ddd534722f745277667a6dcff8c88bb002b20fcf
hmSON-7 <sonhm48021@gmail.com>
2025-04-14 13:37:08 +0900

    update: BOJ_11399

--- java_practice/Rank3/silver_4/_11399ATM.java
+++ java_practice/Rank3/silver_4/_11399ATM.java
@@ -5,23 +5,19 @@ import java.io.*;
 
 public class _11399ATM {
     public static void main(String[] args) throws IOException {
-        // 초기 설정
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[] numsStr = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
         for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(numsStr[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-
-        int sum = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<=i; j++) {
-                sum += arr[j];
-            }
+        int sum = arr[0];
+        for(int i=1; i<n; i++) {
+            arr[i] += arr[i-1];
+            sum += arr[i];
         }
-
         System.out.println(sum);
     }
 }
