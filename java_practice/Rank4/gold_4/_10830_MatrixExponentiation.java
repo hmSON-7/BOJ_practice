39191383d9cedc27d02e34fb0bb457198854a3b5
hmSON-7 <sonhm48021@gmail.com>
2025-05-09 15:20:44 +0900

    update: BOJ_10830

--- java_practice/Rank4/gold_4/_10830_MatrixExponentiation.java
+++ java_practice/Rank4/gold_4/_10830_MatrixExponentiation.java
@@ -3,13 +3,12 @@ package gold_4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class _10830_MatrixExponentiation {
     static int n;
     static long e;
-    static HashMap<Long, int[][]> map = new HashMap<>();
+    static int[][] matrix;
     public static void main(String[] args) throws IOException {
         read();
         int[][] res = solve(e);
@@ -28,23 +27,20 @@ public class _10830_MatrixExponentiation {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         e = Long.parseLong(st.nextToken());
-        int[][] matrix = new int[n][n];
+        matrix = new int[n][n];
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<n; j++) {
                 matrix[i][j] = Integer.parseInt(st.nextToken()) % 1000;
             }
         }
-        map.put(1L, matrix);
     }
 
     private static int[][] solve(long x) {
-        if(x == 1 || map.containsKey(x)) return map.get(x);
+        if(x == 1) return matrix;
         int[][] half = solve(x/2);
         int[][] res = multiply(half, half);
-        if(x%2 != 0) res = multiply(res, solve(1));
-        map.put(x, res);
-        return res;
+        return x%2 == 1 ? multiply(res, matrix) : res;
     }
 
     private static int[][] multiply(int[][] a, int[][] b) {
