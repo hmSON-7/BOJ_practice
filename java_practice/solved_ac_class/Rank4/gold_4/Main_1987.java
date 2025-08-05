9cb39ef36275b4bf5ee13a0f7eaf0c0dee54042a
손홍민 <sonhm48021@gmail.com>
2025-08-05 17:52:12 +0900

    solve: BOJ_1987 알파벳

--- java_practice/solved_ac_class/Rank4/gold_4/Main_1987.java
+++ java_practice/solved_ac_class/Rank4/gold_4/Main_1987.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+package Rank4.gold_4;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1987 {
 	
 	static int r, c, maxCnt = 1;
 	static char[][] map;
