375f1512c1135bef010fad6b64c5abd3a4912c4c
손홍민 <sonhm48021@gmail.com>
2025-09-09 15:21:57 +0900

    :solve: BOJ_17070 파이프 옮기기 1

--- java_practice/solved_ac_class/Rank4/gold_5/Main_17070.java
+++ java_practice/solved_ac_class/Rank4/gold_5/Main_17070.java
@@ -1,15 +1,27 @@
-import java.io.*;
-import java.util.*;
+package Rank4.gold_5;
 
-public class Main {
-    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_17070 {
+	
+	// 맵 크기
 	static int n;
+	// 현재 맵 정보를 비트마스킹으로 저장. 벽이 1
 	static int[] map;
+	// 각 위치마다 도착 가능한 경우의 수를 가로, 대각, 세로 순서로 저장
 	static int[][][] dp;
 	
 	public static void main(String[] args) throws Exception {
 		init();
 		
+		// DP를 이용해 각 경로까지 이동할 수 있는 경우의 수 계산
+		// 가로 파이프 : 가로 + 대각
+		// 대각 파이프 : 가로 + 대각 + 세로
+		// 세로 파이프 : 대각 + 세로
+		// 단, 현 위치가 벽인 경우 무시
+		// 또한 대각 파이프는 위 또는 왼쪽에 벽이 있으면 설치할 수 없음
 		for(int i=1; i<=n; i++) {
 			for(int j=2; j<n; j++) {
 				if((map[i] & 1<<j) != 0) continue;
@@ -19,6 +31,8 @@ public class Main {
 				dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2];
 			}
 		}
+		
+		// 도착점의 모든 경우의 수를 합산하여 출력
 		int sum = dp[n][n-1][0] + dp[n][n-1][1] + dp[n][n-1][2];
 		
 		System.out.println(sum);
@@ -33,13 +47,15 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0; j<n; j++) {
 				int x = Integer.parseInt(st.nextToken());
+				// 벽을 1로 표현
 				if(x == 1) {
 					map[i] |= 1<<j;
 				}
 			}
 		}
 		
+		// 초기 파이프 위치
 		dp[1][0][0] = dp[1][1][0] = 1;
 	}
-	
+
 }
