9b98e756449a582748ef3f356b59165fab2a3147
손홍민 <sonhm48021@gmail.com>
2025-09-09 11:08:39 +0900

    solve: BOJ_12015 가장 긴 증가하는 부분 수열 2

--- java_practice/src/Main.java
+++ java_practice/src/Main.java
@@ -3,25 +3,4 @@ import java.util.*;
 
 public class Main {
 	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int max = 0;
-		int[] arr = new int[n];
-		int[] dp = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			int next = Integer.parseInt(st.nextToken());
-			arr[i] = next;
-			dp[i] = 1;
-			for(int j=0; j<i; j++) {
-				if(arr[i] > arr[j] && dp[i] < dp[j]+1) dp[i]++; 
-			}
-			
-			max = Math.max(max, dp[i]);
-		}
-		
-		System.out.println(max);
-	}
-	
 }
