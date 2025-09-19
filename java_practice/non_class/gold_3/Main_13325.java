cd75fd08d43e3198e87cdf72deddb03e5977191a
손홍민 <sonhm48021@gmail.com>
2025-09-19 16:07:13 +0900

    solve: BOJ_13325 이진 트리

--- java_practice/non_class/gold_3/Main_13325.java
+++ java_practice/non_class/gold_3/Main_13325.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+package gold_3;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_13325 {
 	
 	static int level, n, sum;
 	static int[] arr, dp;
