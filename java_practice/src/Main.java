9dd304e39e6f03f1a6b3d9c30e9803dce9db3e78
DESKTOP-6OO0P6F\SSAFY <sonhm48021@gmail.com>
2026-03-23 17:18:41 +0900

    fix: Main.java 롤백

--- java_practice/src/Main.java
+++ java_practice/src/Main.java
@@ -3,29 +3,4 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        long[][] dp = new long[65][11];
-        Arrays.fill(dp[1], 1);
-        dp[1][10] = 10;
-
-        for(int i=2; i<=64; i++) {
-            dp[i][0] = 1;
-            dp[i][10] = 1;
-            for(int j=1; j<=9; j++) {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-                dp[i][10] += dp[i][j];
-            }
-        }
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        for(int i=0; i<n; i++) {
-            int id = Integer.parseInt(br.readLine());
-            sb.append(dp[id][10]).append("\n");
-        }
-        System.out.println(sb);
-    }
-
 }
